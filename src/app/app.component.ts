104c3f3b8963ab71debab568d1b1bbb531b3a8e7
Jon <jon.a.skinner@me.com>
2017-02-16 00:30:15 -0500

    Added some map pins!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  zoom: number = 10;
+  // Start position
+  lat: number = 40.7128;
+  lng: number = -74.0059;
+
+  markers: marker[] = [
+    {
+      name: 'Company One',
+      lat: 35.7796,
+      lng: -78.6382,
+      draggable: true
+    },
+    {
+      name: 'Company Two',
+      lat: 35.7796,
+      lng: -78.5382,
+      draggable: true
+    },
+    {
+      name: 'Company Three',
+      lat: 35.6796,
+      lng: -78.6382,
+      draggable: true
+    }
+  ];
+
+  constructor(){}
+}
+
+interface marker {
+  name?:string;
+  lat: number;
+  lng: number;
+  draggable: boolean;
 }
